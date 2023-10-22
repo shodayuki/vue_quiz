3849b49558674337e9e7bca1b31270967b6bdc15
shodayuki <yshoda1227@gmail.com>
2023-10-22 20:54:12 +0900

    modify: お知らせ情報APIとVue.jsの連携成功

--- src/components/molecules/home/HomeIntroduction.vue
+++ src/components/molecules/home/HomeIntroduction.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-  import { useInformations } from "@/composables/informations";
-  const { informations } = useInformations()
+import { useInformations } from '@/composables/informations'
+const { informations } = useInformations()
 </script>
 
 <template>
   <section class="home__notice">
-    <h2 class="home__notice-h2">お知らせ情報</h2>
+    <h2 class="home__notice-h2">
+      お知らせ情報
+    </h2>
+
     <dl v-for="(info, index) in informations" :key="index">
       <dt>{{ info.created_at }}</dt>
       <dd>{{ info.information }}</dd>
     </dl>
+
   </section>
 </template>
